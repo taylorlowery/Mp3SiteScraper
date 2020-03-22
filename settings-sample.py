cefaef847d138aaa70e747e69d3dbc408d2b58e5
Taylor Lowery <admiral.taylor@gmail.com>
2020-03-22 10:35:36 -0700

    initial commit!

---
+++ settings-sample.py
@@ -0,0 +1,10 @@
+# path where the downloaded styles should be stored
+# be sure to use double slashes, and include a double slash on the end
+# ex, C:\\Users\\rando\\Documents\\SiteFileDownloads\\
+storagepath = ''
+
+# url of the site from which we are downloading files - ex, 'https://www.#######.com'
+site_url = ''
+
+# url of site's login page - ex, https://www.#######.com/myaccount/login.aspx
+login_url = ''
