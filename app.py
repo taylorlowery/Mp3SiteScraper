8929a89f9049f31ae87eec6b68f1b056914e5719
Taylor Lowery <admiral.taylor@gmail.com>
2020-03-22 13:38:21 -0700

    added PageData class, fixed image download bug

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from PIL import Image
 import credentials
 import settings
 
+from PageData import PageData
+
 # use constants from settings page
 STORAGE_PATH = settings.STORAGE_PATH
 SITE_URL = settings.SITE_URL
@@ -17,7 +19,7 @@ USERNAME = credentials.USERNAME
 PASSWORD = credentials.PASSWORD
 
 
-def clean_string(html_element):
+def clean_html_contents(html_element):
     if html_element is None:
         return ''
     return html_element.text.replace('\r', '').replace('\n', '').strip()
@@ -44,61 +46,72 @@ def generate_login_data(login_page):
     return login_data
 
 
-def attempt_file_download(session, file_id):
-    # urls for details web page and download link
-    details_url = '{site}/details.aspx?id={file_id}'.format(site=SITE_URL, file_id=file_id)
-    dl_url = '{site}/download.aspx?id={file_id}'.format(site=SITE_URL, file_id=file_id)
+def get_file_data_from_page(session, details_url):
 
-    # down
     open_details_page = session.get(details_url)
     details_soup = BeautifulSoup(open_details_page.content, 'lxml')
 
     # get metadata from page
     content = details_soup.find('td', class_='content')
 
+    page_data = PageData()
+
     # Title = Title
-    title = clean_string(content.h1) if content.h1 else ''
+    page_data.title = clean_html_contents(content.h1) if content.h1 else ''
     # Album = Organization
-    album = clean_string(content.find(id='ctl00_ContentPlaceHolder_hypOrganization'))
+    page_data.album = clean_html_contents(content.find(id='ctl00_ContentPlaceHolder_hypOrganization'))
     # Album Artist = Group/Ministry
-    album_artist = clean_string(content.find(id='ctl00_ContentPlaceHolder_panelProductGroups'))
+    page_data.album_artist = clean_html_contents(content.find(id='ctl00_ContentPlaceHolder_panelProductGroups'))
     # Artist = Speaker
-    speaker = clean_string(content.find(id='ctl00_ContentPlaceHolder_hypSpeaker'))
+    page_data.speaker = clean_html_contents(content.find(id='ctl00_ContentPlaceHolder_hypSpeaker'))
     # Genre = Topic
-    genre = clean_string(content.find(id='ctl00_ContentPlaceHolder_hypTopic'))
+    page_data.genre = clean_html_contents(content.find(id='ctl00_ContentPlaceHolder_hypTopic'))
     # Comment = Description + Speaker
-    # comment = content.find(id='').text
+    # page_data['comment'] = content.find(id='').text
     # Description
-    description = content.p.text if content.p else ''
-    # Track  # = Series (have to parse because it's formatted as Part x of a y part series. You can see how I did it in the spreadsheet)
-    raw_track_info = clean_string(content.find(id='ctl00_ContentPlaceHolder_panelSeriesNumber'))
+    page_data.description = content.p.text if content.p else ''
+    # Track  # = Series (have to parse because it's formatted as Part x of a y part series.
+    # You can see how I did it in the spreadsheet)
+    raw_track_info = clean_html_contents(content.find(id='ctl00_ContentPlaceHolder_panelSeriesNumber'))
     track_data = [x for x in raw_track_info.split() if x.isdigit()] if raw_track_info else ''
-    track_number = track_data[0] if len(track_data) == 2 else 0
-    total_tracks = track_data[1] if len(track_data) == 2 else 0
+    page_data.track_number = track_data[0] if len(track_data) == 2 else 0
+    page_data.total_tracks = track_data[1] if len(track_data) == 2 else 0
     # Year / Date = Date
     # year = dl_soup.find(id='').text
 
     # image urls
     speaker_img_url_stub = content.find(id='ctl00_ContentPlaceHolder_imgSpeaker')['src'] if content.find(
         id='ctl00_ContentPlaceHolder_imgSpeaker') else ''
-    speaker_img_url = '{}{}'.format(SITE_URL, speaker_img_url_stub) if speaker_img_url_stub else ''
+    page_data.speaker_image_url = '{}{}'.format(SITE_URL, speaker_img_url_stub) if speaker_img_url_stub else ''
 
     album_img_url_stub = content.find(id='ctl00_ContentPlaceHolder_imgItem')['src'] if content.find(
         id='ctl00_ContentPlaceHolder_imgItem') else ''
-    album_img_url = '{}{}'.format(SITE_URL, album_img_url_stub) if album_img_url_stub else ''
+    page_data.album_image_url = '{}{}'.format(SITE_URL, album_img_url_stub) if album_img_url_stub else ''
+
+    return page_data
+
+
+def attempt_file_download(session, file_id):
+    # urls for details web page and download link
+    details_url = '{site}/details.aspx?id={file_id}'.format(site=SITE_URL, file_id=file_id)
+    dl_url = '{site}/download.aspx?id={file_id}'.format(site=SITE_URL, file_id=file_id)
+
+    page_data = get_file_data_from_page(session=session, details_url=details_url)
 
     # generate name for this mp3 file
-    filename = '{0}_{1}.mp3'.format(file_id, title.replace(' ', '_'))
-    fullpath = STORAGE_PATH + filename
+    filename = '{0}_{1}.mp3'.format(file_id, page_data.title.replace(' ', '_'))
+    full_file_path = STORAGE_PATH + filename
 
     # assuming that worked
     file = session.get(dl_url, allow_redirects=True)
     if file.status_code == 200:
-        with open(fullpath, 'wb') as f:
+        with open(full_file_path, 'wb') as f:
             f.write(file.content)
 
         # get original file metadata
-        audiofile = eyed3.load(fullpath)
+        # TODO: see if more metadata ca be pulled from file
+        # TODO: Save all original data as audiofile tag comment
+        audiofile = eyed3.load(full_file_path)
         original_title = audiofile.tag.title
         original_album = audiofile.tag.album
         original_album_artist = audiofile.tag.album_artist
@@ -108,29 +121,34 @@ def attempt_file_download(session, file_id):
         original_year = audiofile.tag.getBestDate()
 
         # cover image
-        if album_img_url == "":
-            album_image = Image.open(urllib.request.urlopen(album_img_url))
+        if page_data.album_image_url != "":
+            album_image = Image.open(urllib.request.urlopen(page_data.album_image_url))
             album_image_bytes = io.BytesIO()
             album_image.save(album_image_bytes, format='PNG')
             album_image_bytes = album_image_bytes.getvalue()
             audiofile.tag.images.set(3, album_image_bytes, "image/jpeg", u"Description")
 
         # artist image
-        if speaker_img_url == "":
-            artist_image = Image.open(urllib.request.urlopen(speaker_img_url))
+        if page_data.speaker_image_url != "":
+            artist_image = Image.open(urllib.request.urlopen(page_data.speaker_image_url))
             artist_image_bytes = io.BytesIO()
             artist_image.save(artist_image_bytes, format='PNG')
             artist_image_bytes = artist_image_bytes.getvalue()
             audiofile.tag.images.set(8, artist_image_bytes, "image/jpeg", u"Description")
 
         # add appropriate metadata to audio file
-        if audiofile.tag.artist == "":
-            audiofile.tag.artist = speaker
-        if audiofile.tag.album_artist == "":
-            audiofile.tag.album_artist = album_artist
+        # TODO: full comparison of original metadata and metadata from page. Only use page if blank?
+        if audiofile.tag.artist is None or audiofile.tag.artist == "":
+            audiofile.tag.artist = page_data.speaker
+        if audiofile.tag.album_artist is None or audiofile.tag.album_artist == "":
+            audiofile.tag.album_artist = page_data.album_artist
+
+        # attach download and details links
         audiofile.tag.audio_file_url = dl_url
         audiofile.tag.audio_source_url = details_url
-        audiofile.tag.genre = genre
+
+        # definitely use page_data genre
+        audiofile.tag.genre = page_data.genre
         audiofile.tag.save()
 
         print("Download of {0} successful!".format(filename))
